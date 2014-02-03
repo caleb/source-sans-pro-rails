14c3c62db0effc9638634026c51e220a501cbc39
Caleb Land <caleb@land.fm>
2014-02-03 17:00:57 -0500

    initial commit

---
+++ lib/tasks/source_sans_pro_rails_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :source_sans_pro_rails do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '80406bb3a13e457a094a95a1d3d4ecc48104251499363d0d3a5f66c3fd870cd22ec9884930c4496f519fd10f325259ec102c6b0d4074f65da4409230008245b1'
